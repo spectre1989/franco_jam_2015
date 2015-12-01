9989b6db4e80c8b2d3bbfff2a8b0e5f15acff823
Joe Best-Rotheray <spectre1989@gmail.com>
2015-12-01 17:48:24 +0000

    Added a to do list created bones of sound manager that will hopefully replace sound triggering later

---
+++ MoJoe and the D/Assets/code/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour 
+{
+    // Sounds which are different for each player
+    public enum PlayerSoundType
+    {
+        Attack,
+        Die,
+        Jump,
+        Land,
+        Bump
+    }
+
+    // Sounds which are non-player specific
+    public enum SoundType
+    {
+        Rock,
+        Paper,
+        Scissors,
+        Step,
+        MonsterBwargh,
+        MonsterNom
+    }
+
+    [Serializable]
+    public struct PlayerSoundList
+    {
+        public PlayerSoundType soundType;
+        public AudioClip[][] clips;
+    }
+
+    [Serializable]
+    public struct SoundList
+    {
+        public SoundType soundType;
+        public AudioClip[] clips;
+    }
+
+    [SerializeField]
+    private PlayerSoundList[] playerSounds;
+
+    [SerializeField]
+    private SoundList[] sounds;
+}
